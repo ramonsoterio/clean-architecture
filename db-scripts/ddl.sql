b4d4e0c7cbfe4c226d55bb8fadbd6cd1e6c2dc5d
ramonsoterio <ramonsoterio@gmail.com>
2025-03-27 02:44:12 -0300

    Add ddl scripts in docker-compose mysql start

--- db-scripts/ddl.sql
+++ db-scripts/ddl.sql
@@ -1,9 +1,7 @@
-CREATE DATABASE orders;
-
 CREATE TABLE IF NOT EXISTS orders (
     id VARCHAR(255) NOT NULL,
     price FLOAT NOT NULL,
     tax FLOAT NOT NULL,
     final_price FLOAT NOT NULL,
     PRIMARY KEY (id)
-)
+);
